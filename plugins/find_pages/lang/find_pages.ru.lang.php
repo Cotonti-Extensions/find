339251583abe3e83341b6fb9611e6db295a0cfa2
Pavel Tkachenko <dayver@dayver.net>
2013-09-13 23:29:06 +0300

    Create find_pages.ru.lang.php

---
+++ plugins/find_pages/lang/find_pages.ru.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL');
+
+$L['find_page'] = 'Страницы';
+
+$L['find_page_page_title'] = 'Заголовок';
+$L['find_page_page_desc'] = 'Описание';
+$L['find_page_page_text'] = 'Текст';
+
+?>
