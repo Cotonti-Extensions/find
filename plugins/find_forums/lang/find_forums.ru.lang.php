7a3254d4eec8ec05814e5535aca8241bc79ce818
Pavel Tkachenko <dayver@dayver.net>
2013-09-13 23:30:54 +0300

    Create find_forums.ru.lang.php

---
+++ plugins/find_forums/lang/find_forums.ru.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL');
+
+$L['find_forums'] = 'Форумы';
+
+$L['find_forums_ft_title'] = 'Заголовки тем';
+$L['find_forums_ft_desc'] = 'Описания тем';
+$L['find_forums_fp_text'] = 'Посты';
+
+?>
