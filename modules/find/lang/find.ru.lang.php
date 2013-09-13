17b5fa529c133f5e9ec00201455ce6a0492fd749
Pavel Tkachenko <dayver@dayver.net>
2013-09-13 23:26:59 +0300

    Create find.ru.lang.php

---
+++ modules/find/lang/find.ru.lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Russian language file for Find module
+ *
+ * @package find
+ * @version 5.0
+ * @author Dayver
+ * @copyright Copyright (c) Cotonti Team 2013
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$Ls['results'] = array('результаты', 'результат');
+
+$L['Search'] = 'Поиск';
+
+$L['find_all'] = 'Все';
+$L['find_results'] = 'Показано {$onpage} из {$total} {$results}.';
+
+$L['indexer_title'] = 'Индексатор';
+$L['indexer_complete'] = 'Индексация завершена';
+$L['indexer_executed'] = 'Сделано {$queries} запросов за {$time}.';
+
+$L['indexer_reindex_all'] = 'Переиндексировать все ноды';
+$L['indexer_reindex_all_note'] = 'Может занять некоторое время';
+
+$L['indexer_statistics'] = 'Статистика';
+$L['indexer_nodes'] = 'ноды';
+$L['indexer_words'] = 'слова';
+$L['indexer_occurrences'] = 'выражения';
+
+?>
